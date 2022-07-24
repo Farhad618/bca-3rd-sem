31b16572b9c1d6601012faeab86b78ca1d86cb88
Farhad <farhadmolla618@gmail.com>
2022-07-24 23:32:09 +0530

    ca 1 table

--- src/ca1/Table.java
+++ src/ca1/Table.java
@@ -1,4 +1,25 @@
 package ca1;
+import java.util.Scanner;
 
 public class Table {
+    public static void main(String[] args) {
+        int rowOfTable = 3, columnOfTable = 3, i = 1, j = 1;
+        Scanner getInput = new Scanner(System.in);
+
+        System.out.print("Enter a number of rows: ");
+        rowOfTable = getInput.nextInt();
+        System.out.print("Enter a number of columns: ");
+        columnOfTable = getInput.nextInt();
+        System.out.println("Multiplication table is: ");
+
+        do {
+            do {
+                System.out.print(i * j + "\t");
+                j += 1;
+            } while (j <= columnOfTable);
+            j = 1;
+            System.out.println("");
+            i += 1;
+        } while (i <= rowOfTable);
+    }
 }
